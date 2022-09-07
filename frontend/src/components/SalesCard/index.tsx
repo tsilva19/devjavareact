5158b2a5138188f32fdc69b88a5b3225043584fe
Thiago <thiagoalves060@gmail.com>
2022-09-06 21:13:53 -0300

    usestate

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,90 +2,98 @@ import NotificationButton from "../NotificationButton"
 import "./styles.css"
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 function SalesCard() {
- return (
-  <div className="dsmeta-card">
-   <h2 className="dsmeta-sales-title">Vendas</h2>
-   <div>
-    <div className="dsmeta-form-control-container">
-    <DatePicker
-    selected={new Date()}
-    onChange={(date: Date) => {}}
-    className="dsmeta-form-control"
-    dateFormat="dd/MM/yyyy"
-/>
+
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const max = new Date()
+
+  const [minDate, setminDate] = useState(min);
+  const [maxDate, maxsetmaxDate]  = useState(max);
+  
+  return (
+    <div className="dsmeta-card">
+      <h2 className="dsmeta-sales-title">Vendas</h2>
+      <div>
+        <div className="dsmeta-form-control-container">
+          <DatePicker
+            selected={minDate}
+            onChange={(date: Date) => setminDate(date)}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+        <div className="dsmeta-form-control-container">
+          <DatePicker
+            selected={maxDate}
+            onChange={(date: Date) => maxsetmaxDate(date)}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+      </div>
+
+      <div>
+        <table className="dsmeta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="dsmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="dsmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="dsmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+
     </div>
-    <div className="dsmeta-form-control-container">
-    <DatePicker
-    selected={new Date()}
-    onChange={(date: Date) => {}}
-    className="dsmeta-form-control"
-    dateFormat="dd/MM/yyyy"
-/>
-    </div>
-   </div>
-
-   <div>
-    <table className="dsmeta-sales-table">
-     <thead>
-      <tr>
-       <th className="show992">ID</th>
-       <th className="show576">Data</th>
-       <th>Vendedor</th>
-       <th className="show992">Visitas</th>
-       <th className="show992">Vendas</th>
-       <th>Total</th>
-       <th>Notificar</th>
-      </tr>
-     </thead>
-     <tbody>
-      <tr>
-       <td className="show992">#341</td>
-       <td className="show576">08/07/2022</td>
-       <td>Anakin</td>
-       <td className="show992">15</td>
-       <td className="show992">11</td>
-       <td>R$ 55300.00</td>
-       <td>
-        <div className="dsmeta-red-btn-container">
-           <NotificationButton/>
-        </div>
-       </td>
-      </tr>
-      <tr>
-       <td className="show992">#341</td>
-       <td className="show576">08/07/2022</td>
-       <td>Anakin</td>
-       <td className="show992">15</td>
-       <td className="show992">11</td>
-       <td>R$ 55300.00</td>
-       <td>
-        <div className="dsmeta-red-btn-container">
-            <NotificationButton/>
-        </div>
-       </td>
-      </tr>
-      <tr>
-       <td className="show992">#341</td>
-       <td className="show576">08/07/2022</td>
-       <td>Anakin</td>
-       <td className="show992">15</td>
-       <td className="show992">11</td>
-       <td>R$ 55300.00</td>
-       <td>
-        <div className="dsmeta-red-btn-container">
-          <NotificationButton/>
-        </div>
-       </td>
-      </tr>
-     </tbody>
-
-    </table>
-   </div>
-
-  </div>
- )
+  )
 }
 
 export default SalesCard
